1a9e066943a289561405502210e1ff3285317e95
thành <vinhthanhluong@gmail.com>
2021-12-06 18:35:25 +0700

    update page supoort global

--- template/js/modules/FileActiveModule.js
+++ template/js/modules/FileActiveModule.js
@@ -4,4 +4,12 @@ export default function FileActiveModule() {
     $('.tab-link-mobi').on('click', function () {
         $('.tab-link').slideToggle();
     });
+
+    if ($('.support-detail').length) {
+        $('.support-tt').on('click', function () {
+            $(this).parent().toggleClass('active');
+            $(this).next().slideToggle();
+        });
+        
+    }
 }
