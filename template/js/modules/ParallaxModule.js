454e91a216f100f460315ddaacd77488a21e365a
thành <vinhthanhluong@gmail.com>
2021-12-08 22:18:35 +0700

    update rps all + fb

--- template/js/modules/ParallaxModule.js
+++ template/js/modules/ParallaxModule.js
@@ -1,15 +1,18 @@
 export default function ParallaxModule() {
     //parallax 2
     document.addEventListener("mousemove", parallax);
-    function parallax(e){
-        this.querySelectorAll('.parallax').forEach(layer => {
-            const speed = layer.getAttribute('data-speed')
+    function parallax(e) {
+        var width = $(window).width();
+        if (width >= 1200) {
+            this.querySelectorAll('.parallax').forEach(layer => {
+                const speed = layer.getAttribute('data-speed')
 
-            const x = (window.innerWidth - e.pageX*speed)/100
-            const y = (window.innerHeight - e.pageY*speed)/100
+                const x = (window.innerWidth - e.pageX * speed) / 100
+                const y = (window.innerHeight - e.pageY * speed) / 100
 
-            layer.style.transform = `translateX(${x}px) translateY(${y}px)`
-        })
+                layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+            })
+        }
     }
 
 }
