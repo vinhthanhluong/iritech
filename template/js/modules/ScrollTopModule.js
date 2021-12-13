750473c0fc2c6b2a7b0c784a95febec9990d9ca7
thành <vinhthanhluong@gmail.com>
2021-12-13 19:45:50 +0700

    update fb lan 2

--- template/js/modules/ScrollTopModule.js
+++ template/js/modules/ScrollTopModule.js
@@ -1,31 +1,31 @@
 export default function ScrollTopModule() {
 
-    if ($('.scroll-top').length) {
-        $(".scroll-top").click(function () {
-            $('html,body').animate({
-                scrollTop: 0,
-            }, 1000);
-            return false;
-        });
+    // if ($('.scroll-top').length) {
+    //     $(".scroll-top").click(function () {
+    //         $('html,body').animate({
+    //             scrollTop: 0,
+    //         }, 1000);
+    //         return false;
+    //     });
 
-        let lastScrollTop = 0;
-        $(window).scroll(function () {
-            const st = $('html,body').scrollTop();
-            //console.log(st);
+    //     let lastScrollTop = 0;
+    //     $(window).scroll(function () {
+    //         const st = $('html,body').scrollTop();
+    //         //console.log(st);
 
-            if (st <= 10) {
-                $('.scroll-top').removeClass('--active');
-                return;
-            }
+    //         if (st <= 10) {
+    //             $('.scroll-top').removeClass('--active');
+    //             return;
+    //         }
 
-            if (st > lastScrollTop) {
-                $('.scroll-top').removeClass('--active');
-            } else {
-                $('.scroll-top').addClass('--active');
-            }
-            lastScrollTop = st <= 0 ? 0 : st;
-        });
-    }
+    //         if (st > lastScrollTop) {
+    //             $('.scroll-top').removeClass('--active');
+    //         } else {
+    //             $('.scroll-top').addClass('--active');
+    //         }
+    //         lastScrollTop = st <= 0 ? 0 : st;
+    //     });
+    // }
 
     // if ($('.scroll-top-progress').length) {
     //     $(".scroll-top-progress").click(function () {
